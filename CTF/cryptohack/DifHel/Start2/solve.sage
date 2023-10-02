b4b8821bc6f5998f38155462402ec6e077d8fcd2
MNafiz <muhammadnafiz2017@gmail.com>
2023-10-02 14:22:42 +0700

    updated

--- CTF/cryptohack/DifHel/Start2/solve.sage
+++
@@ -1,5 +0,0 @@
-from sage.all import *
-
-p = 28151
-
-print(GF(p).primitive_element())
--- CTF/cryptohack/SSHKEYS/solve.py
+++
@@ -1,7 +0,0 @@
-from Crypto.PublicKey import RSA
-
-with open("key.pub", "r") as f:
-    key = RSA.import_key(f.read())
-    f.close()
-
-print(key.n)
